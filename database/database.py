e49361a621d9fae0a2a4e097de782b8d5217ebbb
MaxGyverTech <maksimfadeenko2@gmail.com>
2020-10-15 23:30:57 +0300

    Fields

--- database/database.py
+++ database/database.py
@@ -1,8 +1,8 @@
 import sqlite3
 
 
-class DB():
-    def __init__(self,dbname:str,debug=False):
+class DB:
+    def __init__(self, dbname: str, debug=False):
         """Создать или открыть объект БД
 
         Args:\n
@@ -12,10 +12,10 @@ class DB():
         self.db = sqlite3.connect(dbname)
         self.sql = self.db.cursor()
         self.debug = debug
-        if self.debug == True:
+        if self.debug:
             print('init good')
-        
-    def createTable(self, table: str,structure: dict):
+
+    def create_table(self, table: str, structure):
         """Создать новую таблицу, если такая уже есть то просто ничего не произойдёт
 
         Args:\n
@@ -24,32 +24,32 @@ class DB():
         """
         self.table = table
         self.columns = structure
-        query = 'CREATE TABLE IF NOT EXISTS '
-        query += table + ' ('
-        lenth = len(structure)
+        query = f'CREATE TABLE IF NOT EXISTS {self.table}'
+        print(query)
+        query += ' ('
+        length = len(structure)
         loop = 0
-        for key in structure:
+        for i in structure:
             loop += 1
-            if loop == lenth:
-                query += key + ' ' + structure[key] + ''
+            if loop == length:
+                query += i.__str__() + ' '
             else:
-              query += key + ' ' + structure[key] + ','
-
+                query += i.__str__() + ','
         query += ')'
 
         self.sql.execute(query)
-        if self.debug == True:
+        if self.debug:
             print(query)
         self.db.commit()
 
-    def gettables(self):
+    def get_tables(self):
         query = 'SELECT name FROM sqlite_master WHERE type = "table"'
         self.sql.execute(query)
-        if self.debug == True:
+        if self.debug:
             print(query)
         return self.sql.fetchall()
-    
-    def del_teable(self,table = ''):
+
+    def del_table(self, table=''):
         if table == '':
             table = self.table
         query = 'DROP TABLE IF EXISTS {}'.format(table)
@@ -58,100 +58,100 @@ class DB():
             print(query)
         self.db.commit()
 
-    def setdefaulttable(self,table: str):
-        self.table= table
+    def set_default_table(self, table: str):
+        self.table = table
         if self.debug == True:
-            print('setted default: '+table)
+            print('setted default: ' + table)
 
-    def havewrite(self, column:str,value,table=None):
+    def have_write(self, column: str, value, table=None):
         if table == None:
             table = self.table
         query = 'SELECT ' + column + ' FROM ' + table + ' WHERE ' + column + ' = "{}"'.format(value)
         self.sql.execute(query)
         if self.sql.fetchone() is None:
-            if self.debug == True:
+            if self.debug:
                 print(query)
                 print(False)
             return False
         else:
-            if self.debug == True:
+            if self.debug:
                 print(query)
                 print(True)
             return True
 
-    def write(self,data:list,table=None):
-        if table == None:
+    def write(self, *data, table=None):
+        if table is None:
             table = self.table
-        
-        query = 'INSERT INTO '+ table
-        
+
+        query = 'INSERT INTO ' + table
+
         query += ' VALUES ('
-        lenth2 = len(data)
+        length2 = len(data)
         loop = 0
         for obj in data:
             loop += 1
-            if loop == lenth2:
+            if loop == length2:
                 query += "'" + str(obj) + "')"
             else:
-              query += "'" + str(obj) + "', "
-        
-        
-        if self.debug == True:
+                query += "'" + str(obj) + "', "
+
+        if self.debug:
             print(query)
         self.sql.execute(query)
         self.db.commit()
 
-    def edit(self,value,idvalue,column,idcolumn=None,table=None):
-        if table == None:
+    def edit(self, value, id_value, column, id_column=None, table=None):
+        if table is None:
             table = self.table
-        if idcolumn == None:
-            idcolumn = column
-        query = 'UPDATE {0} SET {1} = "{2}" WHERE {3} = "{4}"'.format(table, column,value, idcolumn,idvalue)
+        if id_column is None:
+            id_column = column
+        query = 'UPDATE {0} SET {1} = "{2}" WHERE {3} = "{4}"'.format(table, column, value, id_column, id_value)
         self.sql.execute(query)
-        if self.debug == True:
+        if self.debug:
             print(query)
         self.db.commit()
 
-    def delete(self,idvalue,idcolumn,table=None):
-        if table == None:
+    def delete(self, id_value, id_column, table=None):
+        if table is None:
             table = self.table
-        query = 'DELETE FROM {0} WHERE {1} = "{2}"'.format(table,idcolumn,idvalue)
+        query = 'DELETE FROM {0} WHERE {1} = "{2}"'.format(table, id_column, id_value)
         self.sql.execute(query)
-        if self.debug == True:
+        if self.debug:
             print(query)
         self.db.commit()
 
-    def getall(self,value='*',table=None):
-        if table == None:
+    def get_all(self, value='*', table=None):
+        if table is None:
             table = self.table
-        query = f'SELECT {value} FROM '+table
+        query = f'SELECT {value} FROM ' + table
         self.sql.execute(query)
-        if self.debug == True:
+        if self.debug:
             print(query)
         return self.sql.fetchall()
 
-    def getline(self,idvalue,idcolumn,getval=None,returnone = True,table = None):
-        if table == None:
+    def get_line(self, id_value, id_column, getval=None, returnone=True, table=None):
+        if table is None:
             table = self.table
-        if getval == None:
+        if getval is None:
             getval = '*'
-        query = 'SELECT {0} FROM {1} WHERE {2} = "{3}"'.format(getval,table,idcolumn,idvalue)
+        query = 'SELECT {0} FROM {1} WHERE {2} = "{3}"'.format(getval, table, id_column, id_value)
 
         self.sql.execute(query)
-        if self.debug == True:
+        if self.debug:
             print(query)
-        if returnone == True:
+        if returnone:
             return self.sql.fetchone()
         else:
             return self.sql.fetchall()
-    def getcolumns(self,table = None):
-        if table == None:
+
+    def get_columns(self, table=None):
+        if table is None:
             table = self.table
         query = 'PRAGMA table_info({})'.format(table)
         self.sql.execute(query)
         names = [i[1] for i in self.sql.fetchall()]
         # names = self.sql.fetchall()
-        if self.debug == True:
+        if self.debug:
             print(query)
         return names
         # if table == None:
@@ -162,16 +162,14 @@ class DB():
         # if self.debug == True:
         #     print(query)
         # return names
-    def getcolumnstype(self,table = None):
-        if table == None:
+
+    def get_columns_type(self, table=None):
+        if table is None:
             table = self.table
         query = 'PRAGMA table_info({})'.format(table)
         self.sql.execute(query)
         names = [i[2] for i in self.sql.fetchall()]
         # names = self.sql.fetchall()
-        if self.debug == True:
+        if self.debug:
             print(query)
         return names
-
-    
-
--- useDB.py
+++ useDB.py
@@ -1,32 +1,37 @@
-#23.08
-#упрощение оспользования баз данных, by MaxGyverTech  
+# 23.08
+# упрощение оспользования баз данных, by MaxGyverTech
 import sqlite3
 
 from database.database import DB
+from database.fields import *
 
-#создать БД
-#(название файла, нужен ли вывод к терминал(по умолчанию False))
-db = DB('data.db',debug=True)
+# создать БД
+# (название файла, нужен ли вывод к терминал(по умолчанию False))
+db = DB('data.db', debug=True)
 
-#кортеж со столбцами таблици тире их типом данных
-#посмотреть можно тут http://unetway.com/tutorial/sqlite-type-data/
-tabledict = {
-    'name':'TEXT',
-    'id':'BIGINT'
-    }
-#создать таблицу    
-#(имя таблицы, кортеж структуры таблицы)
-db.createTable(table='users',structure=tabledict)
-#проверить еслть ли такая запись в таком то столбце, возвращает false true
-#(столбец, значение для поиска)
-if db.havewrite('id',str(0)) == False:
+# кортеж со столбцами таблици тире их типом данных
+# посмотреть можно тут http://unetway.com/tutorial/sqlite-type-data/
+# table_dict = {
+#     'name': 'TEXT',
+#     'id': 'BIGINT'
+# }
 
-    #и записать данные
-    #(MАССИВ данных для ваписи,важно что в таком же порядке как и в таблице)
-    db.write(['max',0])
+struct = [TextField('name'), IntegerField('id')]
+print(struct)
+# создать таблицу
+# (имя таблицы, кортеж структуры таблицы)
+# db.create_table(table='users', structure=table_dict)
+db.create_table(table='users', structure=struct)
+# проверить еслть ли такая запись в таком то столбце, возвращает false true
+# (столбец, значение для поиска)
+if not db.have_write('id', str(0)):
+    # и записать данные
+    # не актульно(MАССИВ данных для ваписи,важно что в таком же порядке как и в таблице)
+    # вместо массива просто все параметры через запятую
+    db.write('max', 0)
 
-#получить всю таблицу, 23.08, метод работает плохо
-print(db.getall())
+# получить всю таблицу, 23.08, метод работает плохо
+print(db.get_all())
 
 # UPD 25.08: во всех функциях где уместно добавлен НЕОБЯЗАТЕЛЬНЫЙ параметр 'table' для определения таблицы,
 # по умолчанию работает с последне таблицой. Теперь необязательные параметры помечаю при помощи "*"
@@ -35,26 +40,23 @@ print(db.getall())
 
 # UPD 25.08: добавлен метод изменения записи
 
-db.write(['света',2])         # для проверки
-# (значение на которое изменяем, значение по которому ищем какую запись изменить, столбец в котором изменям,*(необязательно)столбец
-# покоторому мы можем искать(по умолчанию тот же в который записываем данные),*таблица)
-db.edit('egor','max','name')
-db.edit(value='катя',idvalue='2',column='name',idcolumn='id',table='users')
-
+db.write('света', 2)  # для проверки
+# (значение на которое изменяем, значение по которому ищем какую запись изменить, столбец в котором изменям,
+# *(необязательно)столбец покоторому мы можем искать(по умолчанию тот же в который записываем данные),*таблица)
+db.edit('egor', 'max', 'name')
+db.edit(value='катя', id_value='2', column='name', id_column='id', table='users')
 
 # UPD 26.08: добавлена функция удаления записи
-db.write(['ivan',10])# для проверки
+db.write('ivan', 10)  # для проверки
 
 # просто для прикола и что бы видеть промежуточный результат
 if input('Delete?(Y/N)').lower() == 'y':
-
     # (значение по которому искать, столбец по которому искать, *таблица)
-    db.delete('ivan','name')
+    db.delete('ivan', 'name')
     # b nen vyt cnfkj ktym gthtrk.xfnm zpsr =)
-    db.write(['kto to',666])
+    db.write('kto to', 666)
     # поодробней ага
-    db.delete(idvalue=666,idcolumn='id',table='users')
-
+    db.delete(id_value=666, id_column='id', table='users')
 
 # 26.08 не понимаю как сделать функцию получения
 
@@ -62,48 +64,57 @@ if input('Delete?(Y/N)').lower() == 'y':
 
 # UPD 26.08: ВСЁ, функция поиска сделана
 
-print(db.getline('egor','name'))  # параметры смотрите ниже
+print(db.get_line('egor', 'name'))  # параметры смотрите ниже
 
 # запустите код ещё раз вы создадите вторую катю это важно
 
-# ну что: (значение по которому ищем, столбец по которому ищем,*можно получить не всю строку а конкретный параметр из неё(по умолчанию вся строка),
-# *можно ли получить НЕ одну строку(по умолчанию только одну)(ДА ДА функция называется получитьстрокУ но может вернуть несколько строк)),*таблица
-a = db.getline(idvalue='катя',idcolumn='name',getval='id',returnone=False,table='users')
+# ну что: (значение по которому ищем, столбец по которому ищем,*можно получить не всю строку а конкретный параметр из
+# неё(по умолчанию вся строка), *можно ли получить НЕ одну строку(по умолчанию только одну)(ДА ДА функция называется
+# получитьстрокУ но может вернуть несколько строк)),*таблица
+a = db.get_line(id_value='катя', id_column='name', getval='id', returnone=False, table='users')
 print(a)
 
 # тестирую
-print(db.getall())
+print(db.get_all())
 
 # UPD 26.08: функция получить всё наконец то работает
 
 # UPD 28.08: пакет упакован и загружен в PyPi !!!
 
 # UPD 31.08: добавлен метод получения столбцов, это было трудно
-print(db.getcolumns(table='users'))
+print(db.get_columns(table='users'))
 
 # UPD 31.08: добавлен метод для получения списка таблиц, пришлось попотеть
-print(db.gettables())
+print(db.get_tables())
 
 # UPD 31.08: добавлен метод установки таблицы по умолчанию, по умолчанию она уже устонавливается при создании
-db.setdefaulttable('users')
+db.set_default_table('users')
 
 # много где добавлен и исправлен debug
 
 # МЫ НЕ УКАЗЫВАЛИ ЭТОТ СТОЛБЕЦ А ОН ЕСТЬ
-print(db.getline(1,'rowid')) 
+print(db.get_line(1, 'rowid'))
 
 # UPD 04.09 для getall добавлен параметр value
-print(db.getall(value='name'))
+print(db.get_all(value='name'))
 
 # UPD 05.09 удаление таблицы
-db.createTable('abc',tabledict) # автоматически ставится по умолучанию
-db.del_teable()
-db.createTable('cba',tabledict)
-db.del_teable(table='cba')
+db.create_table('abc', struct)  # автоматически ставится по умолучанию
+db.del_table()
+db.create_table('cba', struct)
+db.del_table(table='cba')
 
-db.setdefaulttable('users')
+db.set_default_table('users')
 
-print(db.getcolumns())
+print(db.get_columns())
 # UPD 05.09 добавлен метод получения типа данных столбца
-print(db.getcolumnstype())
+print(db.get_columns_type())
 # полность переработан метод получения типов столбцов
+
+# UPD 15.10 все методы переименовыны на читабельны(это было долго)
+
+# UPD 15.10: добавдены поля, больше словарь с типами не нужен. Система создания таблиц всё ещё кривая
+# TextField, IntegerField,RealField,BlobField,NoneField, CustomField
+# (название столбца, *может быть null, *главный ключ)
+
+# UPD 16.10: во write больше не надо подовать массив, просто все значения которые нужны
